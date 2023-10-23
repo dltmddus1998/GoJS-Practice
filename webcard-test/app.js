bdcb375bfe7bc14e0967efb5062a172a5d7fdcd0
이승연 <sylee1274@mz.co.kr>
2023-10-23 16:55:59 +0900

    Feat: web card with lottiefiles test

---
+++ webcard-test/app.js
@@ -0,0 +1,9 @@
+(function () {
+  lottie.loadAnimation({
+    container: document.getElementById('lottie-animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './cloud-animation.json',
+  });
+})();
